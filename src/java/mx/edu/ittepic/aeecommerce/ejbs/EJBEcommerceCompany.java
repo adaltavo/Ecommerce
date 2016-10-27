0d7c38bcf99d3aadbf3dde812d4f11c2bc5e6220
GreRamirezSa <lis_ha_oran@hotmail.com>
2016-10-27 13:27:07 -0600

    Detalle con Update Company
    Actualiza aunque el id no exista e ingresa un nuevo registro

--- src/java/mx/edu/ittepic/aeecommerce/ejbs/EJBEcommerceCompany.java
+++ src/java/mx/edu/ittepic/aeecommerce/ejbs/EJBEcommerceCompany.java
@@ -27,18 +27,63 @@ import mx.edu.ittepic.aeecommerce.util.Message;
  */
 @Stateless
 public class EJBEcommerceCompany {
+
     @PersistenceContext
     private EntityManager entity;
-    
-    
-    
-    
-    public String getCompanies(){
+
+    public String newCompany(String companyname, String neighborhood, String zipcode, String city, String country,
+            String state, String region, String street, String streetnumber, String phone, String rfc, String logo) {
         Message m = new Message();
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        
-         try {
+        //***********
+        try {
+            Company co = new Company();
+
+            //Category c = entity.find(Category.class, Integer.parseInt(categoryid));
+            co.setCompanyname(companyname);
+            co.setNeighborhood(neighborhood);
+            co.setZipcode(zipcode);
+            co.setCity(city);
+            co.setCountry(country);
+            co.setState(state);
+            co.setRegion(region);
+            co.setStreet(street);
+            co.setStreetnumber(streetnumber);
+            co.setPhone(phone);
+            co.setRfc(rfc);
+            co.setLogo(logo);
+
+            entity.persist(co);
+            entity.flush();
+
+            m.setCode(200);
+            m.setMsg("todo hermoso");
+            m.setDetail(co.toString());
+
+        } catch (IllegalArgumentException e) {
+            m.setCode(406);
+            m.setMsg("Error, tipo de dato invalido");
+            m.setDetail(e.getMessage());
+        } catch (TransactionRequiredException e) {
+            m.setCode(403);
+            m.setMsg("Error, prohibido");
+            m.setDetail(e.getMessage());
+        } catch (PersistenceException e) {
+            m.setCode(500);
+            m.setMsg("Error, Algo salió mal :(, vuelve a intentarlo");
+            m.setDetail(e.getMessage());
+        }
+
+        return gson.toJson(m);
+    }
+
+    public String getCompanies() {
+        Message m = new Message();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        try {
             Query q = entity.createNamedQuery("Company.findAll");
             List<Company> companies = q.getResultList();
             m.setCode(200);
@@ -72,6 +117,63 @@ public class EJBEcommerceCompany {
         }
         return gson.toJson(m);
     }
+
+    public String updateCompany(String id, String companyname, String neighborhood, String zipcode, String city, String country,
+            String state, String region, String street, String streetnumber, String phone, String rfc, String logo) {
+        Message m = new Message();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        //***********
+        Company c = entity.find(Company.class, Integer.parseInt(id));
+
+        try {
+            
+            if(c!=null){
+            Company co = new Company();
+
+            //Category c = entity.find(Category.class, Integer.parseInt(categoryid));
+            co.setCompanyname(companyname);
+            co.setNeighborhood(neighborhood);
+            co.setZipcode(zipcode);
+            co.setCity(city);
+            co.setCountry(country);
+            co.setState(state);
+            co.setRegion(region);
+            co.setStreet(street);
+            co.setStreetnumber(streetnumber);
+            co.setPhone(phone);
+            co.setRfc(rfc);
+            co.setLogo(logo);
+
+            entity.persist(co);
+            entity.flush();
+
+            m.setCode(200);
+            m.setMsg("todo hermoso");
+            m.setDetail(co.toString());
+            }else{
+                m.setCode(404);
+                m.setMsg("Error");
+                m.setDetail("No encontrado");
+            }
+
+        } catch (IllegalArgumentException e) {
+            m.setCode(406);
+            m.setMsg("Error, tipo de dato invalido");
+            m.setDetail(e.getMessage());
+        } catch (TransactionRequiredException e) {
+            m.setCode(403);
+            m.setMsg("Error, prohibido");
+            m.setDetail(e.getMessage());
+        } catch (PersistenceException e) {
+            m.setCode(500);
+            m.setMsg("Error, Algo salió mal :(, vuelve a intentarlo");
+            m.setDetail(e.getMessage());
+        }
+
+        return gson.toJson(m);
+    }
+
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 }
